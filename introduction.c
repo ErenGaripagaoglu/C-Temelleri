d5336becd3ad83b8fb8c34b148ae7df3873f4f14
Eren Garipağaoğlu <128929885+ErenGaripagaoglu@users.noreply.github.com>
2025-10-28 20:50:44 +0300

    Add files via upload

---
+++ introduction.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // library import for "standart input-output . header"
+
+int main() { //create main execution function
+    printf("Hello, World!\n"); //print output
+    return 0; //return a placeholder value because the function requires integer value for return
+}
