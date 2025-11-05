19c8c23ff7517000fe27fd4e530aad230fda3e65
Eren Garipağaoğlu <128929885+ErenGaripagaoglu@users.noreply.github.com>
2025-11-05 20:51:45 +0300

    Update ioStreams.c

--- ioStreams.c
+++ ioStreams.c
@@ -21,6 +21,28 @@ int main() {
     scanf("%d-%d-%d", &day, &month, &year);
     // Aim is to get input in "dd-mm-yyyy" format
     printf("%d %d %d \n", day, month, year);
+
+    /* Align Digits */
+    int a=15, b=116;
+    printf("%d\n", a); // Without
+    printf("%d\n", b); // Formatting
+    
+    printf("%3d\n", a); // With
+    printf("%3d\n", b); // Formatting
+    
+    /* Limit Floating Point Presicion */
+    double floatingPoint = 251.7366;
+    printf("%f %.3f %.2f %e %E\n", floatingPoint, floatingPoint, floatingPoint, floatingPoint, floatingPoint);
+    
+    /* Escape Characters
+     * \n = new line
+     * \t = tab space
+     * \a = alert (bell)
+     * \b = backspace
+     * \" = " (text)
+     * \' = ' (text)
+     * \\ = \ (text)
+     */
     
     return 0;
 }
